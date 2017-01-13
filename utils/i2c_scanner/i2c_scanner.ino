6f8bbe4e0bba762d870053d3ca5907c83af9942e
Davis Kulis <dkuulis@gmail.com>
2017-01-13 15:00:44 +0200

    working with multiple I2C buses

--- utils/i2c_scanner/i2c_scanner.ino
+++ utils/i2c_scanner/i2c_scanner.ino
@@ -3,59 +3,75 @@
 //
 // see i2c discussion on  http://www.gammon.com.au/forum/?id=10896
 
-#include <Wire.h>
+#include "i2c_t3.h"
+
+#define BUSES 3
 
 void setup()
 {
-  Wire.begin();
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  while (!Serial); // Leonardo: wait for serial monitor
-  Serial.println(F("\nI2C Scanner"));
+    while (!Serial); // Leonardo: wait for serial monitor
+    Serial.println(F("I2C Scanner"));
+    
+    for (int bus = 0; bus<BUSES; bus++)
+    {
+        Serial.print(F("Initializing bus "));
+        Serial.println(bus);
+        i2c_t3(bus).begin();
+    }
 }
 
 void loop()
 {
-  byte error;
-  byte address;
-  int nDevices;
+    byte error;
+    byte address;
+    int nDevices;
 
-  Serial.println(F("Scanning..."));
-
-  nDevices = 0;
-  for (address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0)
+    for (int bus = 0; bus<BUSES; bus++)
     {
-      Serial.print(F("I2C device found at address 0x"));
-      printhex(address);
+        Serial.print(F("Scanning bus "));
+        Serial.println(bus);
+        nDevices = 0;
 
-      nDevices++;
+        for (address = 1; address < 127; address++ )
+        {
+            // The i2c_scanner uses the return value of
+            // the Write.endTransmisstion to see if
+            // a device did acknowledge to the address.
+
+            i2c_t3(bus).beginTransmission(address);
+            error = i2c_t3(bus).endTransmission();
+            
+            if (error == 0)
+            {
+                Serial.print(F("I2C device found on bus "));
+                Serial.print(bus);
+                Serial.print(F(" at address 0x"));
+                printhex(address);
+
+                nDevices++;
+            }
+            else if (error == 4)
+            {
+                Serial.print(F("Unknow error at address 0x"));
+                printhex(address);
+            }
+        }
+
+        if (nDevices == 0)
+        {
+            Serial.println(F("No I2C devices found on bus "));
+            Serial.println(bus);
+        }
+        else
+        {
+            Serial.println(F("done"));
+        }
     }
-    else if (error == 4)
-    {
-      Serial.print(F("Unknow error at address 0x"));
-      printhex(address);
-    }
-  }
 
-  if (nDevices == 0)
-  {
-    Serial.println(F("No I2C devices found\n"));
-  }
-  else
-  {
-    Serial.println(F("done\n"));
-  }
-
-  delay(5000); // wait 5 seconds for next scan
+    Serial.println();
+    delay(5000); // wait 5 seconds for next scan
 }
 
 void printhex(byte b)
