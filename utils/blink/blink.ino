88e3a73305d7373533558bec1950c35c77b1ec0e
Davis Kulis <dkuulis@gmail.com>
2017-01-13 14:40:12 +0200

    defined all board LEDs

--- utils/blink/blink.ino
+++ utils/blink/blink.ino
@@ -3,20 +3,41 @@
 // Pin 6  has the LED on Teensy++ 2.0
 // Pin 13 has the LED on Teensy 3.0
 
-#define LED 13
+#define LED0 13
+#define LED1 17
+#define LED2 16
+#define LED3 15
+#define LED4 14
+#define LED5 2
+
 #define PULSE 200
 
 void setup()
 {
     // initialize the digital pin as an output.
-    pinMode(LED, OUTPUT);
+    pinMode(LED0, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);    
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    pinMode(LED5, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop()
 {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED0, HIGH);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    digitalWrite(LED5, HIGH);
     delay(PULSE);
-    digitalWrite(LED, LOW);
+    digitalWrite(LED0, LOW);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
     delay(PULSE);
 }
