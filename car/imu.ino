8ae553c40f2acbac07265db89ce79b7562a1b3f6
Davis Kulis <dkuulis@gmail.com>
2017-02-01 16:48:38 +0200

    using interrupts to get IMU data

--- car/imu.ino
+++ car/imu.ino
@@ -3,10 +3,11 @@
 
 // an MPU9250 object with its I2C address
 // of 0x68 (ADDR to GRND) and on Teensy 3.5 bus 0
-MPU9250 IMU(0x68, 0, I2C_PINS_18_19, I2C_PULLUP_EXT);
+static MPU9250 IMU(0x68, 0, I2C_PINS_18_19, I2C_PULLUP_EXT);
 
-float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};
-float d[10];
+static float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};
+static volatile float d[10];
+static volatile int count = 0;
 
 static int imuInterval = IMU_INTERVAL;
 
@@ -34,6 +35,14 @@ void imuSetup()
     };
 
     IMU.setBias(b);
+
+    status = IMU.setFilter(DLPF_BANDWIDTH_184HZ, 9);
+    if (status < 0) 
+    {
+        setupFail("IMU INT unsuccessful");
+    }
+    pinMode(IMU_INT_PIN, INPUT);
+    attachInterrupt(IMU_INT_PIN, getIMU, RISING);    
 }
 
 void imuEeprom(int action, const unsigned long time)
@@ -41,18 +50,28 @@ void imuEeprom(int action, const unsigned long time)
     eepromRW(EEPROM_IMU_INTERVAL, IMU_INTERVAL, imuInterval, action, time);
 }
 
+void getIMU()
+{
+    // get the accel (m/s/s), gyro (rad/s), and magnetometer (uT), and temperature (C) data
+    IMU.getMotion10(d);
+    count++;
+}
+
 void imuLoop(unsigned long time, int mode)
 {
     static unsigned long next;
+    float d2[10];
 
     // is it this time to read IMU
-    if (next < time)
+    if (next < time )
     {
         // set up next ping time
-        next = time + imuInterval;
+        //next = time + imuInterval;
+        next = time + 2;
 
         // get the accel (m/s/s), gyro (rad/s), and magnetometer (uT), and temperature (C) data
-        IMU.getMotion10Unbiased(d);
+        //IMU.getMotion10Unbiased(d);
+        
 
         // Sensors x (y)-axis of the accelerometer/gyro is aligned with the y (x)-axis of the magnetometer;
         // the magnetometer z-axis (+ down) is misaligned with z-axis (+ up) of accelerometer and gyro!
@@ -63,29 +82,39 @@ void imuLoop(unsigned long time, int mode)
         // function to get North along the accel +x-axis, East along the accel -y-axis, and Down along the accel -z-axis.
         // This orientation choice can be modified to allow any convenient (non-NED) orientation convention.
         // Pass gyro rate as rad/sx
-        MadgwickQuaternionUpdate(q, d, IMU.delta);
+        //MadgwickQuaternionUpdate(q, d, IMU.delta);
 
         // print the data
+        if (count > 0) 
         if (mode == MODE_IMU || mode == MODE_RUN || mode == MODE_COUNTDOWN || mode == MODE_RUNOFF)
         {
-            printData(time);
+            cli();
+            int c = count;
+            for (int i = 0; i < 10; i++)
+               d2[i] = d[i];
+            count = 0;
+            sei();
+            printData(time, d2, c);
         }
     }
 }
 
-void printData(unsigned long time)
-{
+void printData(unsigned long time, const float* data, const int cnt)
+{   /*
     float roll;
     float pitch;
     float yaw;
 
-    toEulerianAngle(q, roll, pitch, yaw);
+    toEulerianAngle(q, roll, pitch, yaw);*/
 
-    info(time, "IMU a %f %f %f g %f %f %f m %f %f %f q %f %f %f %f roll %f pitch %f yaw %f d %f\n",
-        d[0], d[1], d[2], // accel
-        d[3], d[4], d[5], // gyro
-        d[6], d[7], d[8], // magnetometer
-        q[0], q[1], q[2], q[3], // quaternion
-        roll, pitch, yaw,
-        IMU.delta);
+//    info(time, "IMU a %f %f %f g %f %f %f m %f %f %f q %f %f %f %f roll %f pitch %f yaw %f d %f\n",
+    info(time, "IMU a %f %f %f g %f %f %f m %f %f %f c %i\n",
+        data[0], data[1], data[2], // accel
+        data[3], data[4], data[5], // gyro
+        data[6], data[7], data[8], // magnetometer
+        //q[0], q[1], q[2], q[3], // quaternion
+        //roll, pitch, yaw,
+        //IMU.delta
+        cnt
+        );
 }
